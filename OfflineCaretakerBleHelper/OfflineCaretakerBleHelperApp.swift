e1412e958bd15d71b96271d3207cbc899828c4fd
Chintan Bawa <chintanbawa@gmail.com>
2026-04-21 18:07:05 -0500

    Initial Commit

---
+++ OfflineCaretakerBleHelper/OfflineCaretakerBleHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineCaretakerBleHelperApp.swift
+//  OfflineCaretakerBleHelper
+//
+//  Created by Chintan Dev on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct OfflineCaretakerBleHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
